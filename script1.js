fe4aed0f79fa23f575502401dda2497717b61481
dhavalsolanki1166 <39427842+dhavalsolanki1166@users.noreply.github.com>
2018-07-18 21:21:49 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,21 @@
+console.log("Recipe details")
+console.log("-----------")
+var recipeName = "Creamy squash linguine";
+var preparationTime = 45;
+var recipeBy = "Jimmy Cutter";
+var isVegetarian = true;
+var calories = 223.7;
+console.log("Recipe Name:"+recipeName);
+console.log("Preparation time:"+preparationTime);
+console.log("Recipe By:"+recipeBy);
+console.log("Is Vegetarian Dish:"+isVegetarian);
+console.log("Calories:"+calories);
+console.log("Recipe details after update");
+console.log("----------")
+console.log("Recipe Name:"+recipeName);
+var preparationTime = 60;
+console.log("Preparation time:"+preparationTime);
+var recipeBy = "Jimmy Cutter George";
+console.log("Recipe By:"+recipeBy);
+console.log("Is Vegetarian Dish:"+isVegetarian);
+console.log("Calories:"+calories);
